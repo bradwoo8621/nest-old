d4f8ef5dacb96ef87f643e5b2ccda92f7443ffcb
brad.wu <bradwoo8621@gmail.com>
2014-12-03 13:17:23 +0800

    initialize

---
+++ arcteryx-logging/arcteryx-logging-logback/src/test/java/com/github/nest/arcteryx/logging/logback/TestLogger.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.github.nest.arcteryx.logging.logback;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author brad.wu
+ */
+public class TestLogger {
+	@Test
+	public void testLogger() {
+		Logger logger = LoggerFactory.getLogger("test-logger");
+		logger.debug("Hello world!");
+		logger = LoggerFactory.getLogger(getClass());
+		logger.debug("Hello world!");
+	}
+}

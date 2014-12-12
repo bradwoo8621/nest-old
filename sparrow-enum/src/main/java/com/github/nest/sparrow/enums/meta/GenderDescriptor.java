120af19a51d9bb3156686c1d5400c27b7d5bd4dd
brad.wu <bradwoo8621@gmail.com>
2014-12-13 00:23:10 +0800

    add GenderDescriptor

---
+++ sparrow-enum/src/main/java/com/github/nest/sparrow/enums/meta/GenderDescriptor.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.github.nest.sparrow.enums.meta;
+
+import com.github.nest.arcteryx.meta.IStaticResourceDescriptor;
+import com.github.nest.arcteryx.meta.ResourceException;
+import com.github.nest.arcteryx.meta.internal.ResourceDescriptor;
+
+/**
+ * Gender resource descriptor
+ * 
+ * @author brad.wu
+ */
+public class GenderDescriptor extends ResourceDescriptor implements IStaticResourceDescriptor {
+	private static final long serialVersionUID = 2700881214747221633L;
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nest.arcteryx.meta.IStaticResourceDescriptor#initialize()
+	 */
+	@Override
+	public void initialize() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+}

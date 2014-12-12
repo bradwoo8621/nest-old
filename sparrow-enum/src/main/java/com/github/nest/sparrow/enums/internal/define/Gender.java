8b2da424ed92ecb96ecd04f3b81070345cb7dc4d
brad.wu <bradwoo8621@gmail.com>
2014-12-12 23:20:05 +0800

    add IGender interface

--- sparrow-enum/src/main/java/com/github/nest/sparrow/enums/internal/define/Gender.java
+++ sparrow-enum/src/main/java/com/github/nest/sparrow/enums/internal/define/Gender.java
@@ -3,6 +3,7 @@
  */
 package com.github.nest.sparrow.enums.internal.define;
 
+import com.github.nest.sparrow.enums.define.IGender;
 import com.github.nest.sparrow.enums.internal.CodedEnumItem;
 
 /**
@@ -10,7 +11,7 @@ import com.github.nest.sparrow.enums.internal.CodedEnumItem;
  * 
  * @author brad.wu
  */
-public class Gender extends CodedEnumItem {
+public class Gender extends CodedEnumItem implements IGender {
 	private static final long serialVersionUID = 1884778534893654701L;
 
 	public Gender(String id, String label, String code) {

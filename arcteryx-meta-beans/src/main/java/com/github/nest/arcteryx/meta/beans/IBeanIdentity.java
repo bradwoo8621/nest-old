2eacab8666bde0f456033c04097fa8544864a596
brad.wu <bradwoo8621@gmail.com>
2014-12-14 23:40:00 +0800

    extends Serializable

--- arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/IBeanIdentity.java
+++ arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/IBeanIdentity.java
@@ -3,11 +3,13 @@
  */
 package com.github.nest.arcteryx.meta.beans;
 
+import java.io.Serializable;
+
 /**
  * bean identity interface
  * 
  * @author brad.wu
  */
-public interface IBeanIdentity {
+public interface IBeanIdentity extends Serializable {
 
 }

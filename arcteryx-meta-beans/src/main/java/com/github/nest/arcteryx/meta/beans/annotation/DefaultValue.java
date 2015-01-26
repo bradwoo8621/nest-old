bdaa4e8cc540bfdc7df486e79ebda7313c175500
brad.wu <bradwoo8621@gmail.com>
2015-01-26 21:42:35 +0800

    some changes

--- arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/annotation/DefaultValue.java
+++ arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/annotation/DefaultValue.java
@@ -3,11 +3,20 @@
  */
 package com.github.nest.arcteryx.meta.beans.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
- * default value
+ * default value.<br>
  * 
  * @author brad.wu
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD })
 public @interface DefaultValue {
 	/**
 	 * default value, separator by comma
--- arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/annotation/SpringCachedBeanParameters.java
+++ arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/annotation/SpringCachedBeanParameters.java
@@ -3,11 +3,25 @@
  */
 package com.github.nest.arcteryx.meta.beans.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.github.nest.arcteryx.meta.beans.internal.SpringCachedBeanDescriptor;
+
 /**
- * spring cached bean parameters
+ * spring cached bean parameters.<br>
+ * when {@linkplain ArcteryxBean#descriptorClass()} is
+ * {@linkplain SpringCachedBeanDescriptor}, add this annotation to type.
+ * {@linkplain CachedBeanParameters} is also necessary.
  * 
  * @author brad.wu
  */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface SpringCachedBeanParameters {
 	/**
 	 * spring context id

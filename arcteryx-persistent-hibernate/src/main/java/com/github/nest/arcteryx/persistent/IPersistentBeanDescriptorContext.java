16735c94965dad51ec2e8cc2a0971a83892d86de
brad.wu <bradwoo8621@gmail.com>
2015-02-02 17:40:16 +0800

    re-add

---
+++ arcteryx-persistent-hibernate/src/main/java/com/github/nest/arcteryx/persistent/IPersistentBeanDescriptorContext.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.github.nest.arcteryx.persistent;
+
+import com.github.nest.arcteryx.meta.beans.IBeanDescriptor;
+
+/**
+ * persistent bean descriptor context
+ * 
+ * @author brad.wu
+ */
+public interface IPersistentBeanDescriptorContext extends IBeanDescriptor {
+
+}

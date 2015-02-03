251badb37a09f5274a06c5415fbde0692c1c2297
brad.wu <bradwoo8621@gmail.com>
2015-02-03 11:22:49 +0800

    change variable name

--- arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/internal/BeanDescriptor.java
+++ arcteryx-meta-beans/src/main/java/com/github/nest/arcteryx/meta/beans/internal/BeanDescriptor.java
@@ -179,15 +179,15 @@ public class BeanDescriptor extends ResourceDescriptor implements IBeanDescripto
 		if (this.propertyDescriptors == null) {
 			synchronized (this) {
 				if (this.propertyDescriptors == null) {
-					List<IBeanPropertyDescriptor> beanDescriptors = new ArrayList<IBeanPropertyDescriptor>();
+					List<IBeanPropertyDescriptor> list = new ArrayList<IBeanPropertyDescriptor>();
 
 					Collection<IPropertyDescriptor> descriptors = this.getDeclaredProperties();
 					for (IPropertyDescriptor descriptor : descriptors) {
 						if (descriptor instanceof IBeanPropertyDescriptor) {
-							beanDescriptors.add((IBeanPropertyDescriptor) descriptor);
+							list.add((IBeanPropertyDescriptor) descriptor);
 						}
 					}
-					this.propertyDescriptors = beanDescriptors;
+					this.propertyDescriptors = list;
 				}
 			}
 		}

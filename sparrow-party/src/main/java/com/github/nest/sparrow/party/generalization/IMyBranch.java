6f4b8814876e783531247bae4c7ce4b709cb35e4
brad.wu <bradwoo8621@gmail.com>
2015-03-23 22:20:51 +0800

    1. fixed the country list file. 2. remove IBranch interface.

--- sparrow-party/src/main/java/com/github/nest/sparrow/party/generalization/IMyBranch.java
+++ sparrow-party/src/main/java/com/github/nest/sparrow/party/generalization/IMyBranch.java
@@ -3,6 +3,7 @@
  */
 package com.github.nest.sparrow.party.generalization;
 
+import com.github.nest.sparrow.party.IOrganization;
 import com.github.nest.sparrow.party.IPartyRole;
 
 /**
@@ -10,7 +11,19 @@ import com.github.nest.sparrow.party.IPartyRole;
  * 
  * @author brad.wu
  */
-public interface IMyBranch extends IBranch, IPartyRole {
+public interface IMyBranch extends IOrganization, IPartyRole {
+	/**
+	 * is head office or not. if true, no parent branch.
+	 * 
+	 * @return
+	 */
+	Boolean isHeadOffice();
+
+	/**
+	 * 
+	 * @param headOffice
+	 */
+	void setHeadOffice(Boolean headOffice);
 	/**
 	 * (non-Javadoc)
 	 * 

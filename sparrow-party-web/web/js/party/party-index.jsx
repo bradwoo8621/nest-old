cab099854169d122e6fb820b50aba3ba3f9b00ae
brad.wu <bradwoo8621@gmail.com>
2015-05-31 22:08:25 +0800

    remove useless projects

--- sparrow-party-web/web/js/party/party-index.jsx
+++
@@ -1,7 +0,0 @@
-var jumbotron = (
-	<Jumbotron>
-		<h1>Hello, world!</h1>
-		<p>This is sparrow-party from NEST@Github.</p>
-	</Jumbotron>
-);
-React.render(jumbotron, document.getElementById("main"));
